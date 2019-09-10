8750f346c99465da64477979609a52dd681bcf14
Ramen Shaman <Gravatacs@gmail.com>
2019-09-10 16:11:11 -0500

    Final edit

--- Lab1/src/WeightInSpace.java
+++ Lab1/src/WeightInSpace.java
@@ -1,3 +1,28 @@
+/* 
+ * TECH 2251
+ * Advanced Programming Technology
+ * Fall 2019
+ * The University of Memphis
+ * 
+ * Assignment:		Weight In Space Program
+ * Developer:		Cameron Hall
+ * Version:			1.0
+ * Compiler:		Java ver. 1.8.0_201
+ * Date:			August 10, 2019
+ * Modified:		August 10, 2019
+ * 
+ * Program Description:
+ * 
+ * This program will calculate the weight of an
+ * object in space based on the objects weight
+ * and the distance from Earth.
+ * 
+ * Example:			The GOES R Satellite weights 6,173 lbs.
+ * 					and orbits the Earth at a distance of 
+ * 					22,300 miles. It's estimated weight
+ * 					in space is 140.57 lbs.
+ */
+
 import java.text.DecimalFormat;
 import java.util.Scanner;
 
@@ -68,7 +93,16 @@ public class WeightInSpace
 		System.out.println("at a distance of " + formatter.format(Height) + " miles from the");
 		System.out.println("Earth will weigh approximately " + formatter.format(SpaceWeight) + " pounds in space.");
 		
+		//Display author info...
+		System.out.println("This program was written by: Cameron Hall");
+		System.out.println("Laboratory Exercise 1");
+		System.out.println("TECH 2251");
+		System.out.println("Advanced Programming Technology");
+		System.out.println("Fall 2019");
+		System.out.println("The University of Memphis");
 		
+		//close scanner instance to prevent memory leaks...
+		userInput.close();
 		
 	}
 
