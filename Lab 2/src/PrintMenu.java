5f8aaa9b9176fcbfd328abb3b731e0e2c24dc4e3
Ramen Shaman <Gravatacs@gmail.com>
2019-09-17 13:58:18 -0500

    Final mock-up

--- Lab 2/src/PrintMenu.java
+++ Lab 2/src/PrintMenu.java
@@ -4,7 +4,7 @@
  * Fall 2019
  * The University of Memphis
  * 
- * Assignment:		Weight In Space Program
+ * Assignment:		Print Menu and ASCII Conversion
  * Developer:		Cameron Hall
  * Version:			1.0
  * Compiler:		Java ver. 1.8.0_201
@@ -13,14 +13,9 @@
  * 
  * Program Description:
  * 
- * This program will calculate the weight of an
- * object in space based on the objects weight
- * and the distance from Earth.
+ * This program will demonstrate a menu selection
+ * while using separate methods for each option.
  * 
- * Example:			The GOES R Satellite weights 6,173 lbs.
- * 					and orbits the Earth at a distance of 
- * 					22,300 miles. It's estimated weight
- * 					in space is 140.57 lbs.
  */
 
 import java.util.Scanner;
@@ -30,31 +25,117 @@ public class PrintMenu
 
 	public static void main(String[] args)
 	{
-		
-		//call description method...
-		ProgramDescription();
-
-		//present categories to users...
-		System.out.println("0. Display Uppercase Letters");
-		System.out.println("1. Display Lowercse Letters");
-		System.out.println("2. Display Numbers 0-9");
-		System.out.println("3. Exit");
-		System.out.println();
-		System.out.println();
-
-		//Prompt user for selection...
-		System.out.print("Enter your selection: ");
-
-
+			boolean exit = false;
+			int userChoice = 0;
+			Scanner keyboard = new Scanner(System.in);
+			
+			do 
+			{
+				
+				//call description method...
+				ProgramDescription();
+	
+				//present categories to users...
+				System.out.println("1. Display Uppercase Letters");
+				System.out.println("2. Display Lowercse Letters");
+				System.out.println("3. Display Numbers 0-9");
+				System.out.println("4. Exit");
+				System.out.println();
+				System.out.println();
+				System.out.print("Enter your selection: ");
+				
+				try
+				{
+					//	ask the user to enter a number...
+					userChoice = keyboard.nextInt();
+					
+					switch(userChoice)
+					{
+						case 1:
+							PrintUppercaseLetters();
+							break;
+						case 2:
+							PrintLowercaseLetters();
+							break;
+						case 3:
+							PrintNumbers();
+							break;
+						case 4:
+							exit = true;
+							break;
+						default:
+							System.out.println();
+							System.out.println("Invalid choice! Please try again.");
+							break; 
+					}
+					
+					
+				}
+				catch(Exception exc)
+				{
+					System.out.println("Oops!  We had an exception!");
+				}
+			}
+			
+			while (!exit);
+			
+			DeveloperInformation();
+			
+			keyboard.close();
 	}
+
 
 	public static void ProgramDescription()
 	{
-	Scanner keybaord = new Scanner(System.in);
-		
+		System.out.println();
 		System.out.println("This program will present different catagories of information");
-		System.out.println("depending on what option the user selects.");
+		System.out.println("depending on what option the user selects. Here are some available options:");
 		System.out.println("");
+	}
+	
+	public static void PrintUppercaseLetters ()
+	{
+		
+		System.out.println();
+		for (int i = 65; i < 91; i++)
+		{
+			System.out.println((char) i);
+		}
+		
+	}
+	
+	
+	public static void PrintLowercaseLetters ()
+	{
+		
+		System.out.println();
+		for (int i = 97; i < 123; i++)
+		{
+			System.out.println((char) i);
+		}
+		
+	}
+	
+	public static void PrintNumbers()
+	{
+		
+		System.out.println();
+		for (int i = 48; i < 58; i++)
+		{
+			System.out.println((char) i);
+		}
+		
+	}
+	
+	public static void DeveloperInformation ()
+	{
+		System.out.println();
+		System.out.println("This program was written by: Cameron Hall");
+		System.out.println("Laboratory Exercise 2");
+		System.out.println("TECH 2251");
+		System.out.println("Advanced Programming Technology");
+		System.out.println("Fall 2019");
+		System.out.println("The University of Memphis");
 	}
 }
 
