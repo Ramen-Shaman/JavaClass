16f00adf7f64c68a28b94210a88dad09aec1d4d3
Ramen Shaman <Gravatacs@gmail.com>
2019-10-17 17:31:20 -0500

    adding in class examples for overriding constructors

---
+++ 2251_F2019_Lecture_8_In_Class/src/Lecture8_Main.java
@@ -0,0 +1,70 @@
+//	TECH 2251
+//	Advanced Programming Technology
+//	Fall 2019
+//	The University of Memphis
+//
+//	Assignment:		In Class Exercise Lecture 8
+//	Developer:		Todd S. Canaday
+//	Version:		1.0
+//	Compiler:		Java ver. 1.8.0_221
+//	Date:			October 12, 2019
+//
+//	Program Description:
+//
+//	In class programming assignment to
+//	develop better understanding of
+//	encapsulation.
+//
+//	The student is to setup and compile this
+//	program for a successful run, then complete
+//	the instructions below.
+
+//	Student:  TO DO ->
+//	need to reference the appropriate class...
+
+public class Lecture8_Main
+{
+	//	object used to grab input from keyboard...
+	static Scanner keyboard = new Scanner(System.in);
+	
+	/**
+	 * Main class where the program is launched from.
+	 * @param args
+	 */
+	public static void main(String args[])
+	{
+		//	Student:  TO DO ->
+		//	create an object of the 'EncapsulationTest' class...
+		
+		
+		//	ask the user to enter a name...
+		System.out.print("Please enter the name:       ");
+		//	Student:  TO DO ->
+		//	store the name entered using the appropriate means...
+		
+		
+		
+		//	ask the user to enter the ID number...
+		System.out.print("Please enter the ID Number:  ");
+		//	Student:  TO DO ->
+		//	store the ID number entered using the appropriate means...
+		
+		
+		
+		//	ask the user to enter the age...
+		System.out.print("Please enter the age:        ");
+		//	Student:  TO DO ->
+		//	store the age entered using the appropriate means...
+		
+		
+		
+		//	Student:  TO DO ->
+		//	display the name, ID number, and age entered in the
+		//	following format:
+		//
+		//	Name:   Test User
+		//	ID Num: 45658LLP
+		//	Age:    56
+		
+    }
+}
