9ad3c72cd72eac62b599eaa08abecda5038ddf63
Ramen Shaman <Gravatacs@gmail.com>
2019-11-19 16:04:25 -0600

    finished Lab 8 by the skin of my goddamn teeth

--- Lab 8/src/CopyFileContents.java
+++ Lab 8/src/CopyFileContents.java
@@ -1,24 +1,69 @@
-import java.io.IOException;
+/* 
+ * TECH 2251
+ * Advanced Programming Technology
+ * Fall 2019
+ * The University of Memphis
+ * 
+ * Assignment:		Copy File Contents
+ * Developer:		Cameron Hall
+ * Version:			1.0
+ * Compiler:		Java ver. 1.8.0_201
+ * Date:			November 12, 2019
+ * Modified:		November 12, 2019
+ * 
+ * Program Description:
+ * 
+ * This program demonstrates the use
+ * of custom exceptions as well as the use of File IO
+ * 
+ * 
+ */
 
+import java.io.IOException;
 import org.uofm.tools.*;
+
 
 public class CopyFileContents 
 {
 
 	public static void main(String[] args) throws IOException
-	{		
-		if(args.length != 0)
+	{				
+		try
 		{
-				ReadDataFile reader = new ReadDataFile();
-				reader.ReadFile(args[0]);
-				
-				WriteDataFile writer = new WriteDataFile();
-				writer.WriteFile(args[1]);
-		}
-		else
-		{
-		}
+			if(args.length > 1)
+			{
+				    String filestuff;
+				    
+					ReadDataFile reader = new ReadDataFile();
+					filestuff = reader.ReadFile(args[0]);
+					
+					WriteDataFile writer = new WriteDataFile();
+					writer.WriteFile(args[1], reader.filecontents);
+					if(writer.writemode ==true)
+					{
+						System.out.println("File successfully copied!");
+					}
+					else
+					{
+						System.out.println("File copy unsuccessful.");
+					}
+					
 		
+			}
+			else
+			{
+				throw(new InvalidFileNameException());
+			}
+		}
+		catch (InvalidFileNameException exc)
+		{
+			
+		}
+		finally
+		{
+			ProgramHelper pH = new ProgramHelper();
+			pH.DeveloperInformation();
+		}
 
 	}
 }
--- Lab 8/src/org/uofm/tools/WriteDataFile.java
+++ Lab 8/src/org/uofm/tools/WriteDataFile.java
@@ -1,25 +1,43 @@
 package org.uofm.tools;
 
-import java.io.BufferedWriter;
-import java.io.FileOutputStream;
+
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
+
 
 public class WriteDataFile 
 {
 
 	public boolean writemode;
 	
-	public boolean WriteFile(String fileName) throws IOException
+	public boolean WriteFile(String fileName, String contents) throws IOException
 	{
-		ReadDataFile rdf = new ReadDataFile();
+	
 		FileWriter Fw = new FileWriter(fileName, false);
 		
-		Fw.write(rdf.filecontents);
+		try
+		{
+		Fw.write(contents);
+		writemode = true;
+		}
 		
-		Fw.close();
+		catch (IOException exc)
+		{
+			writemode = false;
+		}
+		
+		finally
+		{
+			try 
+			{
+				Fw.close();
+			}
+			catch(Exception ex)
+			{
+				writemode = false;
+			}
+		}
+		
 		return writemode;
 	}
 }
